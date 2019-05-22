76296ff38fdd022c3db0d52691d619a4b368b09b
chengkangzai <pycck@hotmail.com>
2019-05-22 22:12:08 +0800

    Detail

---
+++ Assignment/Login/Detail.vb
@@ -0,0 +1,13 @@
+﻿Public Class Detail
+    Private Sub Movie_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+End Class
