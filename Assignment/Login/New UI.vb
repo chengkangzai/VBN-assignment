8ef19f4e7005e7e283a7e7bec3001add3aa7500b
chengkangzai <pycck@hotmail.com>
2019-05-28 14:58:57 +0800

    delete shit and new UI

---
+++ Assignment/Login/New UI.vb
@@ -0,0 +1,28 @@
+﻿Public Class New_UI
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        If Me.WindowState = FormWindowState.Normal Then
+            Me.WindowState = FormWindowState.Maximized
+        Else Me.WindowState = FormWindowState.Normal
+        End If
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click, Button4.Click
+        Dim result As Integer = MessageBox.Show("Are you sure ?", "Comfirmation", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub Button1_MouseHover(sender As Object, e As EventArgs) Handles Button1.MouseHover
+        Button1.ForeColor = maroon
+    End Sub
+End Class
